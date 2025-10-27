5cd6ac645c75a50ae5444ddf4aed02799d8aa829
WilliamClf <wclinharesfilho@gmail.com>
2025-10-26 23:09:51 -0300

    Aula 22/10/2025

--- ecommerce-cms/src/App.tsx
+++ ecommerce-cms/src/App.tsx
@@ -1,6 +1,9 @@
+import { CategoryLayout } from "./cases/categories/components/category-layout"
+
 function App() {
   return (
     <div className="wrapper">
+      <CategoryLayout />
     </div>
   )
 }
